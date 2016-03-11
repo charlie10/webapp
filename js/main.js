2e99677b7a3202357732fc40abd009a062cf550f
charlie mb <charlie_mabariky.1996@hotmail.com>
2016-03-11 21:53:57 +0200

    Add default state when asking URL without hash
    Signed-off-by: charlie mb <charlie_mabariky.1996@hotmail.com>

--- js/main.js
+++ js/main.js
@@ -2,44 +2,54 @@
 
 window.onload = function(){
 
-	var openQuickReports = document.getElementById('open-quick-reports');
-	//openQuickReports.style.border = "solid red"
+	window.location.hash = ( "/quick-reports" ); //by default show quick - reports frame
+	handleTabChange(-1, 0);
 
-
-	document.getElementById('quick-reports').onclick = handleQuickReports;
-	document.getElementById('my-folders').onclick = handleQuickReports;
-	document.getElementById('my-team-folders').onclick = handleQuickReports;
-	document.getElementById('public-folders').onclick = handleQuickReports;
-
-
+	document.getElementById('quick-reports').onclick = handleTabClick;
+	document.getElementById('my-folders').onclick = handleTabClick;
+	document.getElementById('my-team-folders').onclick = handleTabClick;
+	document.getElementById('public-folders').onclick = handleTabClick;
 
 	saveQuickReportButton = document.getElementById('save-quick-report-button');
 	saveQuickReportButton.onclick = handleSaveURLs;	
 
 }
 
+function handleTabClick() {
 
+	handleTabChange(this, 1)
+}
 
-function processAjaxData(response, urlPath){
-     document.getElementById("content").innerHTML = response.html;
-     document.title = response.pageTitle;
-    // window.history.pushState({"html":response.html,"pageTitle":response.pageTitle},"", urlPath);
- }
+/*
+ * Handle navigation between tabs function.
+ * Description: change the url when it's tab click and show the relevant tab frame\
+ * 				or just show the relevant tab on page reload.
+ * Parameters: 
+ 			 - element: the li element was clicked if the function\
+ 			  			was called by tab click, or -1 if it's page\
+ 			  			reload.
+ 			 - isClick: equal to 1 if it's a click call or 0 if it\
+ 			 			is a reload call.
+ * Returns: nothing.
+*/
 
+function handleTabChange(element, isClick) {
 
- window.onpopstate = function(){
- 	var w = window;
- 	var h = w.history;
- 	var ps =h.pushState;
- 	debugger;
- //	window.history.pushState("", "Title", "/nurl");
- }
+	if( isClick === 1 ) {
+		window.location.hash = ( "/" + element.id );
+		var frameElementPrefix = element.id;
+	}
+	else {
+		var hashString = window.location.hash;
+		if(hashString == '') {
+			return ;
+		}
+		var frameElementPrefix = hashString.substring(2);
+	}
 
- function handleQuickReports() {
-
- 	window.location.hash = ( "/" + this.id );
-
- }
+	var currentTab = document.getElementById( frameElementPrefix + "-frame" );
+	currentTab.style.display = "block";
+}
 
 
  function handleSaveURLs() {
