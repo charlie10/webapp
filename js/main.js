f38006f2777f726e8c0258c973fcd92688d65f14
charlie mb <charlie_mabariky.1996@hotmail.com>
2016-03-12 01:28:43 +0200

    Add iframe and design tabs
    Change tabs colors on click.
    Add iframe to Quick Reports tab
    Add infrastructure to local storage handling.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
 
+var tabsIds 	 = [
+						"quick-reports",
+						"my-folders",
+						"my-team-folders",
+						"public-folders"
+];
+
+var urlsNamesIds = [
+						"name1",
+						"name2",
+						"name3"
+]
+
+var urlsIds 	 = [
+						"url1",
+						"url2",
+						"url3"
+]
+
 
 window.onload = function(){
 
-	window.location.hash = ( "/quick-reports" ); //by default show quick - reports frame
-	handleTabChange(-1, 0);
+	var hashString = window.location.hash;
+	if ( hashString == '' ) {
+		window.location.hash = ( "/quick-reports" ); //by default show quick - reports frame
+	}
 
-	document.getElementById('quick-reports').onclick = handleTabClick;
-	document.getElementById('my-folders').onclick = handleTabClick;
-	document.getElementById('my-team-folders').onclick = handleTabClick;
-	document.getElementById('public-folders').onclick = handleTabClick;
+	for ( i = 0; i < tabsIds.length; ++i ) {
+		document.getElementById(tabsIds[i]).onclick = handleTabClick;
+	}
+
+	for ( i = 0; i < urlsNamesIds.length; ++i) {
+		document.getElementById(urlsNamesIds[i]).onchange = handleNameChange;
+	}
+
+	handleTabChange(-1, 0);
 
 	saveQuickReportButton = document.getElementById('save-quick-report-button');
 	saveQuickReportButton.onclick = handleSaveURLs;	
 
 }
+
 
 function handleTabClick() {
 
@@ -32,12 +59,20 @@ function handleTabClick() {
  			 			is a reload call.
  * Returns: nothing.
 */
-
 function handleTabChange(element, isClick) {
 
 	if( isClick === 1 ) {
 		window.location.hash = ( "/" + element.id );
 		var frameElementPrefix = element.id;
+
+	for ( i = 0; i < tabsIds.length; ++i ) {
+		TabLi = document.getElementById( tabsIds[i] );
+		TabFrame = document.getElementById( tabsIds[i] + '-frame' );
+		TabFrame.style.display = "none";
+		TabLi.style.backgroundColor = "#646464";
+		TabLi.style.color = "white";
+	}
+
 	}
 	else {
 		var hashString = window.location.hash;
@@ -48,11 +83,51 @@ function handleTabChange(element, isClick) {
 	}
 
 	var currentTab = document.getElementById( frameElementPrefix + "-frame" );
+	var currentTabLi = document.getElementById( frameElementPrefix );
 	currentTab.style.display = "block";
+	currentTabLi.style.backgroundColor = "#EBEBEB";
+	currentTabLi.style.color = "black";
 }
 
 
+function handleNameChange() {
+	var urlNumber = this.id.substring(4);
+	var thisUrl = document.getElementById( "url" + urlNumber );
+
+	if( ( this.value == undefined ) || ( this.value == '' ) ) {
+		thisUrl.required = false;
+		thisUrl.style.border = "none";
+	}
+	else {
+			thisUrl.required = true;
+			//thisUrl.style.border = "solid red";
+	}
+
+}
+
  function handleSaveURLs() {
 
+ 	//alert('save form')
 
+	for ( i = 0; i < urlsNamesIds.length; ++i) {
+		currentUrlName = document.getElementById(urlsNamesIds[i])
+		currentUrl = document.getElementById(urlsIds[i])
+		if ( currentUrlName.value != '' ) {
+			if ( currentUrl.value != '') {
+				if ( isUrl( currentUrl.value ) ) {	
+					localStorage.setItem("", "");
+				}
+			}
+		}
+	}
+
+ 	//return false;
  }
+
+/*
+check wheter str is in url format or not
+*/
+function isUrl(str) {
+   var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+   return regexp.test(str);
+}
